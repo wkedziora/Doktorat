9ef3a1b4c65851fd490c2dafdb7ca44c2680e642
Wojciech Kedziora <wojciech.kedziora@wl.sggw.pl>
2017-10-25 18:27:13 +0100

    new loading and site index calculation (mean height)

--- skrypt.R
+++ skrypt.R
@@ -1,127 +1,120 @@
-rm(list=ls())
-setwd("D:\\Praca\\Badania\\Doktorat\\")
-
-library(dplyr)
-library(dbplyr)
-library(tidyr)
-library(plotly)
-library(data.table)
-library(rgdal)
+library(tidyverse)
+library(tmap)
+# library(rgdal)
 library(sp)
-library(FSA)
-library(RColorBrewer)
-library(EnvStats)
+# library(FSA)
+# library(RColorBrewer)
+# library(EnvStats)
 
-dane <- read.csv2("dane.txt", dec = ".", stringsAsFactors = TRUE, sep="\t")
+# data loading -----
+sites_loading <- cols(rok_w_cyklu = col_factor(levels = NULL), # for propoer data loading I need to provide column classes
+                    rdlp = col_factor(levels = NULL),
+                    nadl = col_factor(levels = NULL),
+                    kraina = col_factor(levels = NULL),
+                    gat_pan_pr = col_factor(levels = NULL),
+                    b_pion_pow_pr = col_factor(levels = NULL),
+                    tsl = col_factor(levels = NULL),
+                    okr_tsl = col_factor(levels = NULL),
+                    stan_siedl = col_factor(levels = NULL)
+                    )
+sites_area_gps <- read_tsv("sites_area_gps.txt", col_types = sites_loading)
 
-str(dane)
-colnames(dane) <- tolower(colnames(dane))
+trees_loading <- cols(gat = col_factor(levels = NULL), 
+                      war = col_factor(levels = NULL)) 
+trees <- read_tsv("trees.txt", col_types = trees_loading)
 
-dane %>% 
-  dplyr::select(id, nr_punktu, nr_cyklu, nr_podpow, gat, wiek, war, h, d13, rdlp, nadl, kraina, gat_pan_pr, wiek_pan_pr, 
-                b_pion_pow_pr, tsl, ukszt_ter, okr_tsl, stan_siedl, pow_a) %>% 
-  dplyr::filter(nr_cyklu == 2, gat_pan_pr == "SO", gat == "SO", war == 1) %>%
-  dplyr::mutate(nadles = paste0(rdlp, "-", nadl)) -> dane2 # tworzenie klas wieku
-summary(dane2)
-str(dane2)
-hist(dane2$h)
-qqnorm(dane2$h)
-boxplot(dane2$h, outline = FALSE)
-plot(ecdf(dane2$h))
-summary(dane2$h, na.rm = TRUE)
+# data wrangling -----
+ggplot(trees, aes(h)) + geom_freqpoly(binwidth = 1)
+ggplot(trees, aes(x = "", y = h)) + geom_boxplot()
+ggplot(trees, aes(sample = h)) + stat_qq()
+ggplot(trees, aes(h)) + stat_ecdf(geom = "step")
+summary(trees$h, na.rm = TRUE)
 
-dane2 %>%
-  group_by(nr_podpow) %>%
+# no of sample plots with different no of h measured
+trees %>%
   filter(!is.na(h)) %>%
-  summarise(n = n_distinct(h)) -> liczba_h
-
-liczba_h %>%
+  group_by(nr_podpow) %>%
+  summarise(n = n_distinct(h)) %>%
   group_by(n) %>%
-  summarise(z = n_distinct(nr_podpow)) -> liczba_pow
+  summarise(z = n_distinct(nr_podpow)) %>%
+  ggplot(aes(n, z)) +
+  geom_bar(stat = "identity")
 
-plot(liczba_pow)
-
-
-###########
-### OLD ###
-###########
-o <- 0.7445
-r <- -0.4531
-
-dane2 %>%
+trees %>%
   group_by(nr_podpow) %>%
-  filter(!is.na(d13)) %>%
-  summarise(kraina = mean(kraina),
-            tsl = mean(tsl),
-            rdlp = first(rdlp),
-            kodn = first(nadles),
-            n_d = n_distinct(d13), # liczba pierśnic na powierzchni próbnej
-            pow = mean(pow_a), # wielkość powierzchni próbnej - co z małymi powierzchniami?
-            zageszczenie = n_d/(pow/10000),
-            wskaznik = ceiling(pow/100), # liczba arów powierzchni = liczba drzew do SI
-            D100 = sqrt(mean((d13[order(-d13)[1:wskaznik]])^2, na.rm = TRUE))/10, # średnia kwadratowa najgrubszych pierśnic
-            Dg = sqrt(mean(d13^2))/10, # średnia kwadratowa wszystkich pierśnic na powierzchni
-            n_h = n_distinct(h), # liczba pomierzonych wysokości
-            Hg = weighted.mean(h, d13, na.rm = TRUE), # średnia wysokość ważona pierśnicą
-            b = o*(Hg^r), 
-            a = (Dg/(sqrt(Hg-1.3)))-b*Dg, 
-            H100 = (D100/(a+b*D100))^2+1.3, # wysokość 100 najgruszych drzew na ha
-            wiek = mean(wiek), # wiek powierzchni
-            A = (wiek/(22.222222 + 0.777778 * wiek))^2, # standaryzowana funkcja wzrostu wysokości
-            SI = H100/A) %>% 
-  dplyr::mutate(kw = cut(wiek, breaks=seq(0, 260, by=20))) %>%
-  filter(pow > 199) %>%
-  arrange(desc(SI)) -> wynik
+  filter(!is.na(d13), gat == "SO") %>%
+  left_join(., sites_area_gps, by = "nr_podpow") %>%
+  filter(!is.na(h)) %>%
+  group_by(nr_podpow) %>%
+  summarise(n = n_distinct(h)) %>%
+  group_by(n) %>%
+  summarise(z = n_distinct(nr_podpow)) %>%
+  ggplot(aes(n, z)) +
+  geom_bar(stat = "identity")
 
-levels(wynik$kw) <- c("I", "II", "III", "IV", "V", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st")
-wynik$kraina <- factor(wynik$kraina)
 
 ####################
-### Wersja Sochy ###
+### Wersja Sochy ### DO POPRAWY
 ####################
 
-o <- 0.7445
-r <- -0.4531
+values <- tibble(
+  o = 0.7445,
+  r = -0.4531,
+  b1 = 1.381,
+  b3 = 32.947,
+  b2 = 4679.9
+)
 
-b1 <- 1.381
-b3 <- 32.947
-b2 <- 4679.9
-
-dane2 %>%
+trees %>%
   group_by(nr_podpow) %>%
-  filter(!is.na(d13)) %>%
-  summarise(nr_punktu = first(nr_punktu),
-            kraina = mean(kraina),
-            tsl = first(tsl),
-            ukszt_ter = first(ukszt_ter),
-            rdlp = first(rdlp),
-            kodn = first(nadles),
-            n_d = n_distinct(d13), # liczba pierśnic na powierzchni próbnej
-            pow = mean(pow_a), # wielkość powierzchni próbnej - co z małymi powierzchniami?
-            zageszczenie = n_d/(pow/10000),
-            wskaznik = ceiling(pow/100), # liczba arów powierzchni = liczba drzew do SI
-            D100 = sqrt(mean((d13[order(-d13)[1:wskaznik]])^2, na.rm = TRUE))/10, # średnia kwadratowa najgrubszych pierśnic
-            Dg = sqrt(mean(d13^2))/10, # średnia kwadratowa wszystkich pierśnic na powierzchni
+  filter(!is.na(d13), gat == "SO") %>%
+  # left_join(., sites_area_gps, by = "nr_podpow") %>%
+  summarise(n_d = n_distinct(d13), # liczba pierśnic na powierzchni próbnej
+            # pow = mean(pow), # wielkość powierzchni próbnej - co z małymi powierzchniami?
+            # zageszczenie = n_d/(pow/10000),
+            # wskaznik = ceiling(pow/100), # liczba arów powierzchni = liczba drzew do SI
+            # D100 = sqrt(mean((d13[order(-d13)[1:wskaznik]])^2, na.rm = TRUE))/10, # średnia kwadratowa najgrubszych pierśnic
+            # Dg = sqrt(mean(d13^2))/10, # średnia kwadratowa wszystkich pierśnic na powierzchni
             n_h = n_distinct(h), # liczba pomierzonych wysokości
-            Hg = weighted.mean(h, d13, na.rm = TRUE), # średnia wysokość ważona pierśnicą
-            b = o*(Hg^r), 
-            a = (Dg/(sqrt(Hg-1.3)))-b*Dg, 
-            H = (D100/(a+b*D100))^2+1.3, # wysokość 100 najgruszych drzew na ha
+            H = weighted.mean(h, d13, na.rm = TRUE), # średnia wysokość ważona pierśnicą
+            # b = o*(Hg^r), 
+            # a = (Dg/(sqrt(Hg-1.3)))-b*Dg, 
+            # H = (D100/(a+b*D100))^2+1.3, # wysokość 100 najgruszych drzew na ha
             wiek = mean(wiek), # wiek powierzchni
-            SI=H*((100^b1)*((wiek^b1)*(H-b3+(((H-b3)^2)+(2*b2*H)/(wiek^b1))^0.5)+b2))/((wiek^b1)*((100^b1)*(H-b3+(((H-b3)^2)+(2*b2*H)/(wiek^b1))^0.5)+b2))) %>% 
+            SI=H*((100^values$b1)*((wiek^values$b1)*(H-values$b3+(((H-values$b3)^2)+(2*values$b2*H)/(wiek^values$b1))^0.5)+values$b2))/((wiek^values$b1)*((100^values$b1)*(H-values$b3+(((H-values$b3)^2)+(2*values$b2*H)/(wiek^values$b1))^0.5)+values$b2))) %>% 
   dplyr::mutate(kw = cut(wiek, breaks=seq(0, 260, by=20))) %>%
-  filter(pow > 199) %>%
-  arrange(desc(SI)) -> wynik2
+  # filter(pow > 199) %>%
+  arrange(desc(SI)) -> site_index
 
-levels(wynik2$kw) <- c("I", "II", "III", "IV", "V", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st")
-wynik2$kraina <- factor(wynik2$kraina)
-# backup wyników
-# write.table(wynik2, "result.txt", sep="\t", row.names=FALSE)
+levels(site_index$kw) <- c("I", "II", "III", "IV", "V", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st", "VI i st")
 
 ### połączenie ze współrzędnymi GPS
 
-colnames(gps.coord) <- tolower(colnames(gps.coord))
-wynikGPS <- dplyr::left_join(wynik2, gps.coord, by = "nr_punktu")
+site_index_gps <- dplyr::left_join(site_index, sites_area_gps, by = "nr_podpow") %>% na.omit()
+
+ggplot(site_index, aes(SI)) + geom_freqpoly(binwidth = 1)
+ggplot(site_index, aes(x = "", y = SI)) + geom_boxplot()
+ggplot(site_index, aes(sample = SI)) + stat_qq()
+ggplot(site_index, aes(SI)) + stat_ecdf(geom = "step")
+summary(site_index$SI, na.rm = TRUE)
+
+coordinates(site_index_gps) <- ~ lon + lat #adding sptial relationship
+proj4string(site_index_gps) <- "+init=epsg:4326" #adding WGS84 projection
+
+tm_shape(site_index_gps) + tm_dots(col = "SI", size = 0.05)
+
+
+# 
+
+
+
+
+
+
+
+
+
+
 wynikGPS %>%
   select(point_no = nr_punktu, subpoint_no = nr_podpow, lat = szerokosc, long = dlugosc, SI, tsl, ukszt_ter) -> wynikGPS
   
@@ -330,16 +323,3 @@ dane %>%
 #  - pierwsze wyniki
 #      - dwie grupy: wielkości bonitacji w różnych podziałach
 #      - badanie związku z różnymi elementami środowiska
-
-### SOSNA A
-b1 <- 1.46138930116412 #B
-b2 <- 10.2415777396998 #C
-b3 <- -4.52984673991139 #D
-### SOSNA B
-# b1 <- 1.18309592261132 #B
-# b2 <- 13.9780231187729 #C
-# b3 <- -3024.12789944235 #D
-wiek <- 25 #E
-wys <- 12 #F
-
-SoA <- wys*(100^b1*(wiek^b1*(-b3+wys+sqrt((-b3+wys)^2+2*wys*exp(b2)/(wiek^b1)))+exp(b2))/(wiek^b1*(100^b1*(-b3+wys+sqrt((-b3+wys)^2+2*wys*exp(b2)/(wiek^b1)))+exp(b2))))
