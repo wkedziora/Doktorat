90236fcd80beaaf64b93e68c32a5a61db793b931
Wojciech Kedziora <wojciech.kedziora@wl.sggw.pl>
2017-10-26 18:00:26 +0100

    adding citation

--- CRU.R
+++ CRU.R
@@ -31,6 +31,7 @@ plot(Poland.wgs84, border = "black", add = TRUE)
 # van der Schrier G, Barichivich J, Briffa KR and Jones PD (2013) A scPDSI-based global data set of dry and wet spells for 1901-2009. J. Geophys. Res. Atmos. 118, 4025-4048 (10.1002/jgrd.50355).
 # Osborn TJ, Barichivich J, Harris I, van der Schrier G and Jones PD (2017) Monitoring global drought using the self-calibrating Palmer Drought Severity Index [in "State of the Climate in 2016"]. Bulletin of the American Meteorological Society 98, S32-S33 (doi:10.1175/2017BAMSStateoftheClimate.1) (available here).
 # Osborn TJ, Barichivich J, Harris I, van der Schrier G and Jones PD (2016) Monitoring global drought using the self-calibrating Palmer Drought Severity Index [in "State of the Climate in 2015"]. Bulletin of the American Meteorological Society 97, S32-S36 (available here).
+# Wells N, Goddard S and Hayes MJ (2004) A self-calibrating Palmer Drought Severity Index. Journal of Climate 17, 2335-2351 (doi:10.1175/1520-0442(2004)017<2335:ASPDSI>2.0.CO;2).
 
 pdsi_name <- "d://Praca//Badania//Doktorat//CRU//scPDSI.cru.3.25.bams2017.GLOBAL.1901.2016.nc"
 
