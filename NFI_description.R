21c31fa53b580d65ca843b5cb4aa09b052715155
Wojciech Kedziora <wojciech.kedziora@wl.sggw.pl>
2018-08-30 14:11:07 +0200

    looking for missing data for the whole database

--- NFI_description.R
+++ NFI_description.R
@@ -6,7 +6,9 @@ library(tidyverse)
 library(feather)
 library(tmap)
 library(rgdal)
+library(raster)
 library(sp)
+library(finalfit)
 
 # data loading -----
 sites <- read_feather(paste0(getwd(), "/data/WISL/sites.feather")) ### comment one
@@ -15,65 +17,67 @@ area <- read_feather(paste0(getwd(), "/data/WISL/area.feather"))
 gps <- read_feather(paste0(getwd(), "/data/WISL/gps.feather"))
 
 # sample plot data wrangling -----
-sites %>% summarise(n = n_distinct(plot_no))
-sites %>% summarise(n = n_distinct(subplot_no))
+sites %>% summarise(n = n_distinct(nr_punktu))
+sites %>% summarise(n = n_distinct(nr_podpow))
 
  # species
 sites %>% 
-        group_by(species = plot_species) %>% 
+        mutate(gat_pan_pr = fct_lump(gat_pan_pr, n = 10, other_level = "Inne")) %>%
+        group_by(species = gat_pan_pr) %>% 
         summarise(n = n()) %>% 
         mutate(proc = 100*n/sum(n)) %>%
-        mutate(species = fct_reorder(species, -proc)) %>%
-        mutate(species = fct_lump(species, n = 5)) %>%
+        mutate(species = fct_reorder(species, proc, .desc = TRUE)) %>%
+        # mutate(species = fct_infreq(species)) %>%
         arrange(desc(n)) %>%
         ggplot(., aes(x = species, y = proc)) + 
         geom_col() + 
         labs(x = "gatunki", y = "[%]") + 
-        theme_minimal() 
+        # theme_minimal() +
+        scale_x_discrete(na.value = "red")
 # NA to głównie szczątkowe/dopełniające powierzchnie próbne
 
 # habiatats
 sites %>% 
-        group_by(habitat = habitat) %>% 
+        group_by(tsl) %>% 
         summarise(n = n()) %>% 
         mutate(proc = 100*n/sum(n)) %>%
-        mutate(habitat = fct_reorder(habitat, -proc)) %>%
-        mutate(habitat = fct_lump(habitat, n = 5)) %>%
+        mutate(tsl = fct_reorder(tsl, -proc)) %>%
+        mutate(tsl = fct_lump(tsl, n = 5)) %>%
         arrange(desc(n)) %>%
-        ggplot(., aes(x = habitat, y = proc)) + 
+        ggplot(., aes(x = tsl, y = proc)) + 
         geom_col() + 
         labs(x = "siedliska", y = "[%]") + 
         theme_minimal()
 
 # fertility
 sites %>% 
-        group_by(fertility = fertility) %>% 
+        group_by(zyzn) %>% 
         summarise(n = n()) %>% 
         mutate(proc = 100*n/sum(n)) %>%
         arrange(desc(n)) %>%
-        ggplot(., aes(x = fertility, y = proc)) + 
+        ggplot(., aes(x = zyzn, y = proc)) + 
         geom_col() + 
         labs(x = "żyzność", y = "[%]") + 
         theme_minimal()
 
 #humidity1
 sites %>% 
-        group_by(humidity1 = humidity1) %>% 
+        group_by(wilg1) %>% 
         summarise(n = n()) %>% 
         mutate(proc = 100*n/sum(n)) %>%
         arrange(desc(n)) %>%
-        ggplot(., aes(x = humidity1, y = proc)) + 
+        ggplot(., aes(x = wilg1, y = proc)) + 
         geom_col() + 
         labs(x = "wilgotność", y = "[%]") + 
         theme_minimal()
 
 #humidity2
 sites %>% 
-        group_by(humidity2 = humidity2) %>% 
+        group_by(wilg2) %>% 
         summarise(n = n()) %>% 
         mutate(proc = 100*n/sum(n)) %>%
         arrange(desc(n)) %>%
-        ggplot(., aes(x = humidity2, y = proc)) + 
+        ggplot(., aes(x = wilg2, y = proc)) + 
         geom_col() + 
         labs(x = "wilgotność w grupach", y = "[%]") + 
         theme_minimal()
@@ -89,25 +93,75 @@ sites %>%
         labs(x = "klasa wieku", y = "[%]") + 
         theme_minimal()
 
-## ----- gps ----------------------------------------------------------------------------------------
-sites_gps <- dplyr::left_join(sites, gps, by = "plot_no") #%>% na.omit()
+### SPATIAL PATTERN OF MISSING DATA ---------------------------------------------------------------------------
+# sites_gps <- dplyr::left_join(sites, gps, by = "nr_punktu") #%>% na.omit()
+sites_gps <- sites %>% 
+        mutate(gat_pan_pr = fct_lump(gat_pan_pr, n = 10, other_level = "Inne")) %>%
+        # filter(is.na(gat_pan_pr)) %>% # only for map of NA's
+        dplyr::left_join(., gps, by = "nr_punktu")
 
-### backup write/load site_index_gps ---------------------------------------------------------------------------------
+### backup write/load site_index_gps ---------------------------------------------------------------------------
 # write_feather(site_index_gps, paste0(getwd(), "/data/WISL/site_index_gps.feather"))
 # site_index_gps <- read_feather(paste0(getwd(), "/data/WISL/site_index_gps.feather"))
 
-coordinates(sites_gps) <- ~ lon + lat #adding sptial relationship
+coordinates(sites_gps) <- ~ dlugosc + szerokosc #adding sptial relationship
 proj4string(sites_gps) <- "+init=epsg:4326" #adding WGS84 projection
 
-## ----- region-map ----------------------------------------------------------------------------------------
-data(World, rivers)
-# vistula <- subset(rivers, name == "Vistula")
-Poland <- subset(World, name == "Poland")
+## ----- region-map ---------------------------------------------------------------------------
+Region <- readOGR(dsn = "D:\\Praca\\Badania\\Doktorat\\shp", layer = "Region")
+Poland <- subset(Region, COUNTRY == "Poland")
 plot_map_dots <- function(shape, feature) {
-        tm_shape(World, bbox = "Poland", projection="longlat", is.master = TRUE) + tm_borders() +
-                # tm_shape(vistula) + tm_lines(col = "steelblue", lwd = 4) +
-                tm_shape(shape) + tm_dots(col = feature, size = 0.05, n = 5) +
+        tm_shape(Region, bbox = "Poland", projection="longlat", is.master = TRUE) + tm_borders() +
+                tm_shape(shape) + tm_dots(col = feature, size = 0.5, n = 5) +
                 tm_style("white", legend.position = c("left", "bottom"))
 }
 
-plot_map_dots(sites_gps, "plot_species")
+plot_map_dots(sites_gps, "gat_pan_pr")
+
+bb_hire <- as(extent(sites_gps), "SpatialPolygons")
+crs(bb_hire) <- crs(sites_gps)
+
+r <- raster(bb_hire, ncol = 100, nrow = 100)
+rc <- rasterize(sites_gps@coords, r, fun = "count")
+plot(rc)
+points(sites_gps)
+plot(Poland, add = TRUE)
+
+tm_shape(rc) + tm_raster() +
+        tm_shape(Region, bbox = "Poland", projection="longlat", is.master = TRUE) + tm_borders()
+
+### checking for missing data ---------------------------------------------------------------------------
+dependent <- c("plot_species")
+# explanatory <- c("plot_age", "habitat", "habitat_status", "kw", "ownership")
+explanatory <- c("kw", "owner")
+sites %>% ff_glimpse()
+
+# Identify missing values in each variable
+sites %>% missing_plot()
+
+# Look for patterns of missingness
+sites %>% missing_pattern(dependent, explanatory)
+# sites %>% summary_factorlist(dependent, explanatory, na_include = TRUE, p = TRUE)
+
+# Check for associations between missing and observed data
+sites %>% missing_pairs(dependent, explanatory)
+sites %>% missing_pairs(dependent, explanatory, position = "fill")
+# sites %>% finalfit(dependent, explanatory, metrics=TRUE)
+
+# checking the same for "trees"
+dependent_trees <- c("age")
+# explanatory <- c("plot_age", "habitat", "habitat_status", "kw", "ownership")
+explanatory_trees <- c("h", "owner", "kw", "dbh", "fertility")
+trees %>% ff_glimpse()
+
+# Identify missing values in each variable
+trees %>% missing_plot()
+
+# Look for patterns of missingness
+trees %>% missing_pattern(dependent_trees, explanatory_trees)
+# trees %>% summary_factorlist(dependent, explanatory, na_include = TRUE, p = TRUE)
+
+# Check for associations between missing and observed data
+trees %>% missing_pairs(dependent_trees, explanatory_trees)
+trees %>% missing_pairs(dependent_trees, explanatory_trees, position = "fill")
+trees %>% finalfit(dependent_trees, explanatory_trees, metrics = TRUE)
