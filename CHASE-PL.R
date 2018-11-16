193cc93399639c31c5a27812a35e06852eedc683
Wojciech Kedziora <wojciech.kedziora@wl.sggw.pl>
2018-11-16 16:39:26 +0100

    some cleaning

--- CHASE-PL.R
+++ CHASE-PL.R
@@ -1,13 +1,13 @@
 library(raster)
-library(rgdal)
-library(dismo)
+# library(rgdal)
+# library(dismo)
 # library(rJava)
 library(ncdf4)
+library(sf)
 library(tidyverse)
 
 worldclim_data <- list.files(path = "D:\\Praca\\Badania\\Doktorat\\data\\WorldClim", pattern='\\.tif$', full.names = TRUE)
 worldclim <- stack(worldclim_data)
-plot(worldclim[[5]])
 
 envirem_data <- list.files(path = "D:\\Praca\\Badania\\Doktorat\\data\\envirem", pattern='\\.tif$', full.names = TRUE)
 envirem <- stack(envirem_data)
@@ -16,20 +16,25 @@ envirem <- stack(envirem_data)
 # print(precip)
 # r_precip <- stack("D:\\Praca\\Badania\\Doktorat\\data\\CHASE-PL\\PreciForMonths.nc", varname = "precipitation_amount")
 
-gps_coord <- read_tsv("Chris/resultGPS.txt")
-gps_coord <- gps_coord[-10927,] #one is out of climatic dataset range
-coordinates(gps_coord) <- ~ long + lat
-proj4string(gps_coord) <- "+init=epsg:4326" #adding WGS84 projection
+# gps_coord <- read_tsv("Chris/resultGPS.txt")
+gps <- st_as_sf(read_tsv("Chris/resultGPS.txt"), coords = c('long', 'lat'), crs = "+init=epsg:4326")
+# gps_coord <- gps_coord[-10927,] #one is out of climatic dataset range
+# coordinates(gps_coord) <- ~ long + lat
+# proj4string(gps_coord) <- "+init=epsg:4326" #adding WGS84 projection
 # set.seed(35)
 # gps_coord_sample <- gps_coord[sample(nrow(gps_coord), 10), ]
 
-data <- data.frame(coordinates(gps_coord),
-                   gps_coord$SI, 
-                   raster::extract(worldclim, gps_coord),
-                   raster::extract(envirem, gps_coord))
-names(data)[3] <- c("SI")
+data <- tibble(gps$SI, raster::extract(worldclim, gps), raster::extract(envirem, gps))
+names(data)[1] <- c("SI")
 
 old_data <- data
+
+res <- cor(old_data, use = "complete.obs")
+round(res, 2)
+
+library(corrplot)
+corrplot(res, type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
+
 
 coordinates(data) <- ~ long + lat
 proj4string(data) <- "+init=epsg:4326" #adding WGS84 projection
@@ -39,9 +44,9 @@ for (i in names(old_data)[-c(1:3)]) {
 plot(old_data[[i]], old_data$SI)
 }
 
-linear_model <- lm(SI ~ wc2.0_bio_5m_04 + wc2.0_bio_5m_05 + wc2.0_bio_5m_12, data = data) #u Sochy R = 0,29
+linear_model <- lm(SI ~ bio_04 + bio_05 + bio_12, data = data) #u Sochy R = 0,29
 summary(linear_model)
 
 library(gam)
-gam_model <- gam(SI ~ wc2.0_bio_5m_04 + wc2.0_bio_5m_05 + wc2.0_bio_5m_12, data = data) #u Sochy R = 0,29
+gam_model <- gam(SI ~ bio_05 + bio_12, data = data) #u Sochy R = 0,29
 summary(gam_model)
